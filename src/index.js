b7a6321e611f4e566f7a3a3ea8147c711420c0c5
Zihad Malik <zihadm654@gmail.com>
2021-12-23 20:10:51 +0600

    just finished the ui part

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const menu = document.querySelector("#menu")
+const menuBar = document.querySelector(".nav__center")
+
+  menu.addEventListener("click",()=>{
+    if(menu.classList.contains("active")){
+      menu.classList.remove("active")
+      menu.classList.add("close")
+      menuBar.classList.add("open")
+    }else if(menu.classList.contains("close")){
+      menu.classList.add("active")
+      menu.classList.remove("close")
+      menuBar.classList.remove("open")
+    }
+  })
